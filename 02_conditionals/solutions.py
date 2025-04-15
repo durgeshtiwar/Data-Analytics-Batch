5c7d3f0e2cc2b07ec945dd28df6b1c4fdb0cedfd
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-04-15 16:11:18 +0530

    Submit Solution

--- 02_conditionals/solutions.py
+++ 02_conditionals/solutions.py
@@ -1,10 +1,44 @@
-# Classify a person's age group: Child (< 13), Teenager (13-19), Adult (20-59), Senior (60+).
-age_group = int(input('Enter Your Age group'))
-if(age_group < 13):
-  print("Child")
-elif(age_group < 19):
-  print("Teenager")
-elif(age_group < 59):
-  print("Adult")
-else:
-  print("Senior")
+# age_group = int(input('Enter Your Age group'))
+# if(age_group < 13):
+#   print("Child")
+# elif(age_group < 19):
+#   print("Teenager")
+# elif(age_group < 59):
+#   print("Adult")
+# else:
+#   print("Senior")
+
+
+# age_group = int(input("What is your age ?"))
+# if(age_group >= 18):
+#   print("Your Ticket price is",12 - 2, "$")
+# else:
+#   print("Your Ticket price is",8 - 2, "$")
+
+
+# Problem: Assign a letter grade based on a student's score: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
+# Problem: Determine if a fruit is ripe, overripe, or unripe based on its color. (e.g., Banana: Green - Unripe, Yellow - Ripe, Brown - Overripe)
+# Problem: Suggest an activity based on the weather (e.g., Sunny - Go for a walk, Rainy - Read a book, Snowy - Build a snowman).
+
+# weather = input("How  is weather ??")
+# if(weather == "Sunny"):
+#   print("This is the Perfect weather Go For Walk")
+# elif( weather == "Rainy"):
+#   print("THis is Perfect weather For Read a book")
+# else:
+#   print("THis is Perfect weather For Build a snowman")
+
+# Problem: Choose a mode of transportation based on the distance (e.g., <3 km: Walk, 3-15 km: Bike, >15 km: Car).
+# Problem: Customize a coffee order: "Small", "Medium", or "Large" with an option for "Extra shot" of espresso.
+# Problem: Check if a password is "Weak", "Medium", or "Strong". Criteria: < 6 chars (Weak), 6-10 chars (Medium), >10 chars (Strong).
+# Problem: Determine if a year is a leap year. (Leap years are divisible by 4, but not by 100 unless also divisible by 400).
+
+# year = int(input("Enter Year"))
+# if((year % 100 == 0) and (year % 400 == 0)):
+#   print("This is Leap Year")
+# elif(year % 4 == 0):
+#   print("This is a Leap Year")
+# else:
+#   print("This is Not A Leap Year")
+
+# Problem: Recommend a type of pet food based on the pet's species and age. (e.g., Dog: <2 years - Puppy food, Cat: >5 years - Senior cat food).
