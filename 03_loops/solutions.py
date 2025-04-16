a22d5809ae671e74ae3a2da06cefbb60cc5adf4e
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-04-16 21:36:39 +0530

    Changes in loops

--- 03_loops/solutions.py
+++ 03_loops/solutions.py
@@ -29,8 +29,51 @@
 #   print(str[str_length - i - 1])
 
 # str = input("Enter any string")
-# list_1 = []
-# for i in range(0, len(str)):
-#   for j in range(i, len(str)):
-#     if str[i] == str[j]:
-#       break
+# for i in str:
+#   if str.count(i) == 1:
+#     print("First Non Reapeated Char is", i)
+#     break
+    
+# Problem: Keep asking the user for input until they enter a number between 1 and 10.
+
+# while(1):
+#   number = int(input("Enter any Number\n"))
+#   if(number > 1 and number < 10):
+#     print("This is a Valid Number")
+#     break
+#   else:
+#     print("This is not A Valid No.")
+
+# Problem: Check if a number is prime.
+# number = int(input("Enter any number"))
+# is_Prime = True
+# for i in range(2, number):
+#   if (number%i == 0):
+#     is_Prime = False
+#     break
+# if(is_Prime):
+#   print("This is a Prime Number")
+# else:
+#   print("This is not a Prime Number")
+
+# Problem: Check if all elements in a list are unique. If a duplicate is found, exit the loop and print the duplicate.
+# items = ["apple", "banana", "orange", "apple", "mango"]
+# unique_item = set()
+# for i in items:
+#   if i in unique_item:
+#     print(i,"is the duplicate item of List")
+#     break
+#   unique_item.add(i)
+
+# Problem: Implement an exponential backoff strategy that doubles the wait time between retries, starting from 1 second, but stops after 5 retries.
+
+# import time
+# wait_Time = 1
+# max_Retries = 5
+# attempt = 0
+
+# while attempt < max_Retries :
+#   print("Wait Time = ",wait_Time, "Attempt = ",attempt + 1)
+#   time.sleep(wait_Time)
+#   wait_Time = wait_Time * 2
+#   attempt = attempt + 1
