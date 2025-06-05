0ea02df1688df447b29dbbf574f5a1fe6e0be3c4
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-06-05 11:24:33 +0530

    Basic Array and list

---
+++ PYTHON/Basic_DSA/array_and_list.py
@@ -0,0 +1,35 @@
+# from array import *
+
+# array_1 = array('i', [5, 6, 8, 9, 10, 2, 3, 12, 18])
+# print(type(array_1))
+# print(sorted(array_1))
+
+# list_1 = [5, 7, 8, "Durgesh", True, 3.90, "Shubham"]
+# for i in list_1[:]:
+#   if(type(i) != int):
+#     list_1.remove(i)
+# print((list_1))
+
+# def is_prime(num):
+#   if(num < 2):
+#     return False
+#   for i in range(2, num):
+#     if(num % i == 0):
+#       return False
+#   return True
+
+# number = int(input("Enter any number"))
+# num = 2
+# count = 0
+# list_1 = []
+# while(count < number):
+#   if(is_prime(num)):
+#     list_1.append(num)
+#     count = count + 1
+#     num = num + 1
+#   else:
+#     num = num + 1
+
+# print(type(list_1))
+# print(list_1)
+
