e4db45bca0e7021ddfb0856f875785b5c456007f
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-06-06 12:51:15 +0530

    classes and object basics

---
+++ PYTHON/Basic_DSA/classes_and_object.py
@@ -0,0 +1,29 @@
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name 
+#     self.age = age
+
+#   def show(self):
+#     print(f"{self.name} {self.age}")
+
+# P1 = Person("Durgesh", 21)
+# P1.show()
+  
+# class Circle:
+#   def __init__(self, redius):
+#     self.redius = redius 
+#   def set(self,redius):
+#     self.redius = redius
+#   def get(self):
+#     return self.redius
+#   def get_area(self):
+#     return 3.14 * self.redius * self.redius
+#   def get_circumference(self):
+#     return 2 * 3.14 * self.redius
+  
+  
+# c1 = Circle(6)
+# c1.set(5)
+# print(c1.get())
+# print(c1.get_area())
+# print(c1.get_circumference())
