846e2508b9759a7beaba91fea877c24474c4f1ed
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-06-11 19:25:58 +0530

    stack Decorator

--- PYTHON/04_functions/decorator.py
+++ PYTHON/04_functions/decorator.py
@@ -1,9 +1,9 @@
-# def decorator_func(func):
-#   def wrapper():
-#     print("This will execute before function")
-#     func()
-#     print("This will execute after function")
-#   return wrapper
+def decorator_func(func):
+  def wrapper(*args, **kargs):
+    print("This will execute before function")
+    func(*args, **kargs)
+    print("This will execute after function")
+  return wrapper
 
 # @decorator_func
 # def say_hi():
@@ -15,12 +15,13 @@ def timer(func):
   def wrapper(*args, **kargs):
     start = time.time()
     func(*args, **kargs)
-    print("Time taken by the function is ",time.time() - start)
+    print("Time taken by this function is",time.time() - start)
   return wrapper
 
 @timer
+@decorator_func
 def list_parser(list):
-  time.sleep(2)
+  time.sleep(1)
   print(list)
   for i in list:
     print(i)
