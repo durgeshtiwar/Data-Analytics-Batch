2200a7c359d50225ceba3cc24e1bf55b1274f529
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-09-13 19:05:24 +0530

    Refactor chai.py to remove commented code and enhance performance with matrix operations

--- PYTHON/01_basics/chai.py
+++ PYTHON/01_basics/chai.py
@@ -1,2 +1,17 @@
-from hello_chai import chai
-chai("ginger tea")
+# from hello_chai import chai
+# chai("ginger tea")
+
+import numpy as np
+import time
+n = 5000
+m1 = [[e for e in range(n)] for i in range(n)]
+m2 = [[e for e in range(n)] for i in range(n)]
+start = time.time()
+for i in range(n):
+  for j in range(n):
+    m1[i][j]+m1[i][j]
+
+
+
+end = time.time()
+print("%6f sec"%(end-start))
