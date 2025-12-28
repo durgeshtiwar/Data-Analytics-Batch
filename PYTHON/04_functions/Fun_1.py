6f6434f04ab7677493eecd66283b7ce288536982
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-12-28 23:00:43 +0530

    Refactor and clean up code: remove unused functions and improve dict_to_list implementation

--- PYTHON/04_functions/Fun_1.py
+++ PYTHON/04_functions/Fun_1.py
@@ -1,18 +1,47 @@
-# def check(List_1):
-#   emptyList = []
-#   if(type(List_1) == list):
-#     for i in List_1:
-#       emptyList.append(i)
-#   return emptyList
+# you have to write a fun which will take string and return a len of it without using a inbuilt fun len
 
-
-# List_1  = [1,2,3,4,5, [True, False], "Durgseh", "Shubham", (10,12,"Tiwari")]
-# print(check(List_1))
-
-def print_str(row, column):  
-  for i in range(1, row):
-    for j in range(0, i):
-      print("*", end=" ")
-    print("\n")
+# def Check_string(str):
+#   cnt = 0
+#   for i in str:
+#     cnt = cnt + 1
+#   return cnt
     
-print_str(10,10)
+# print("Length of String is", Check_string("Durgesh Tiwari"))
+
+# 2 . write a fun which will be able to print an index of all premitive element which you will pass
+
+# def find_index(list_1):
+#   for i in list_1:
+#     print(i,"it's index no is",list_1.index(i), "and it's type is",type(i))
+    
+# list_1 = [2,5,8,9,20,"Duregsh", True,
+#           ["xyz", True, False, 5],
+#           (2,5,7,8,9)]
+# find_index(list_1)
+
+# 3 . Write a fun which will take input as a dict and give me out as a list of all the values even in case of 2 level nesting it should work.
+
+def dict_to_list(dict_1):
+  list_1 = []
+  if(type(dict_1) == dict):
+    for key, value in dict_1.items():
+      if(type(value) == dict):
+        for subkey, subavlue in value.items():
+          list_1.append(subkey)
+          list_1.append(subavlue)
+      else:  
+        list_1.append(key)
+        list_1.append(value)
+  return list_1
+
+dict_1 = {
+  "Name":"Durgesh",
+  "Age":23,
+  "Qualification":{
+    "Course":"B.Tech",
+    "Branch":"CSE",
+    "Completion Year" : 2024
+  }
+}
+
+print(dict_to_list(dict_1))
